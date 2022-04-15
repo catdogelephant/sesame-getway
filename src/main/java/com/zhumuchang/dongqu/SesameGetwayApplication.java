c3582dd5bd3c195376445137caf94f9606526a88
sunxiao <13964402805@36kr.com>
2022-04-15 15:05:02 +0800

    集成Eureka成功

--- src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
+++ src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
@@ -3,9 +3,12 @@ package com.zhumuchang.dongqu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//服务端的启动类，可以接受别人注册进来
-//@EnableEurekaServer
+/**
+ * 服务端的启动类，可以接受别人注册进来
+ */
+@EnableEurekaServer
 @Slf4j
 @SpringBootApplication
 public class SesameGetwayApplication {
