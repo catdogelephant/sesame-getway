3756b43ca73946c98cdf7561789e5c3ad8f8dcb3
sunxiao <13964402805@36kr.com>
2022-04-21 16:16:32 +0800

    集成GateWay

--- src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
+++ src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//服务端的启动类，可以接受别人注册进来
-//@EnableEurekaServer
 @Slf4j
 @SpringBootApplication
 public class SesameGetwayApplication {
