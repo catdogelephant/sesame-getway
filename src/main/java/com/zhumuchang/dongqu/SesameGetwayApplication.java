dd7d5b20d4c2bf6a72bc28778beb472062bf880e
sunxiao <13964402805@36kr.com>
2022-04-22 10:27:58 +0800

    集成Nacos成功

--- src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
+++ src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
@@ -3,7 +3,9 @@ package com.zhumuchang.dongqu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @Slf4j
 @SpringBootApplication
 public class SesameGetwayApplication {
