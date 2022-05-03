3ce8280de8e7c83cb6af4ac4ceeb284fd3c53c26
sunxiao <13964402805@36kr.com>
2022-05-03 10:57:57 +0800

    整合 Spring Boot Admin 服务端

--- src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
+++ src/main/java/com/zhumuchang/dongqu/SesameGetwayApplication.java
@@ -1,10 +1,15 @@
 package com.zhumuchang.dongqu;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @EnableAdminServer spring boot admin服务端
+ */
+@EnableAdminServer
 @EnableDiscoveryClient
 @Slf4j
 @SpringBootApplication
